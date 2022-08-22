b5466c539709febfc3af4360d7a07adfee18cdc0
11AlexanderB <bezhenar93@internet.ru>
2022-08-22 14:13:36 +0300

    4

---
+++ 4zadanie/Program.cs
@@ -0,0 +1,16 @@
+﻿Console.Write("Введите число: ");
+int CurNum = 1; 
+int N = Convert.ToInt32(Console.ReadLine());
+
+while(CurNum<=N)
+{
+    if(CurNum%2==0)
+    {
+        Console.WriteLine(CurNum);
+        CurNum = CurNum+1;
+    }
+    else
+    {
+        CurNum = CurNum+1;
+    }  
+}
