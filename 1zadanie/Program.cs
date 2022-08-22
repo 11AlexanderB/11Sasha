7e9ba01b4556a400ed9ac71aabd61a893304b80b
11AlexanderB <bezhenar93@internet.ru>
2022-08-22 14:02:38 +0300

    1

---
+++ 1zadanie/Program.cs
@@ -0,0 +1,15 @@
+﻿Console.Write("Введите число A: ");
+int A = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите число B: ");
+int B = Convert.ToInt32(Console.ReadLine());
+
+if(A>B)
+{
+    Console.Write("Наибольшее число: ");
+    Console.Write(A);
+}
+else
+{
+    Console.Write("Наибольшее число: ");
+    Console.Write(B);
+}
