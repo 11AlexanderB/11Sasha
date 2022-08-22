895b9b6c08914715a4181cac503cc2812cc7199d
11AlexanderB <bezhenar93@internet.ru>
2022-08-22 14:09:12 +0300

    3

---
+++ 3zadanie/Program.cs
@@ -0,0 +1,13 @@
+﻿Console.Write("Введите число: ");
+int Num = Convert.ToInt32(Console.ReadLine());
+
+if(Num%2==0)
+{
+    Console.Write(Num);
+    Console.Write("   -Является четным числом");
+}
+else
+{
+    Console.Write(Num);
+    Console.Write("   -Является нечетным числом");
+}
